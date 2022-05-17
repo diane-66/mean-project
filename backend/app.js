7d5c5291e4e4d7f3ed43591e326e419f96be147e
diane-66 <d.dechaignon@gmail.com>
2022-05-16 20:02:13 -0400

    fetching initial posts from the server with Angular HTTP Client & CORS

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,36 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res, next) => {
-  console.log('first middleware')
+app.use((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+    )
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+    )
   next()
 })
 
-app.use((req, res, next) => {
-  res.send('Hello from express')
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'tsj3',
+      title: 'First-server-side post',
+      content: 'this is coming from the server'
+    },
+    {
+      id: 't837f',
+      title: 'Second-server-side post',
+      content: 'this is coming from the server'
+    }
+  ]
+  res.status(200).json({
+    message: 'Posts fetched succesfully',
+    posts: posts
+  })
 })
 
 module.exports = app
